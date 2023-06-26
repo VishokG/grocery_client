2a73dd57093b4d46bbd1772c1b515d50a63c6962
G Vishok <vishokprasad27@gmail.com>
2023-06-26 11:03:57 +0530

    Added Items in checkout

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import "../styles/checkout.css"
 import Item from './Item';
 
 const Checkout = () => {
+
+  const cart = useSelector(state => state.cart.value);
+  console.log(cart);
+
   return (
     <div className="checkout-container center_flex">
         <div className="checkout-wrapper">
           <h3 className="checkout-title">Checkout</h3>
           <div className="checkout">
               <div className="checkout-items">
-                  <Item />
-                  {/* <Item /> */}
-                  {/* <Item /> */}
+                  {cart.map(item => <Item values={item} />)}
               </div>
-              <div className="checkout-total"></div>
+              <div className="checkout-total">
+                
+              </div>
           </div>
         </div>
     </div>
